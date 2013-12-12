396a8d1e097f871a271680c1733a937e131a8265
Joe Chee <joe.chee@gmail.com>
2013-12-12 10:15:36 +0800

    rewrote webrtc code to support multiple peers

--- app.js
+++ app.js
@@ -20,16 +20,16 @@ var webSocketServer = new WebSocketServer({server: server});
 var connections = {};
 var connectionIDCounter = 0;
 
+
+var RTCRequestCallbacks = {};
+
 server.listen(process.env.PORT || 8080);
 
-webSocketServer.broadcast = function (client, msg) {
-	for (var i in this.clients) {
-		if (this.clients[i] !== client) {
-			this.clients[i].send(msg);
-		}
-	}
-};
 
+var RNG = function () {};
+RNG.generate = function () {
+	return Math.floor(Math.random() * 1000000000);
+};
 
 webSocketServer.on('connection', function (connection) {
 	console.log("client connected");
@@ -39,14 +39,14 @@ webSocketServer.on('connection', function (connection) {
 	connection.on('message', function (msg) {
 		msg = JSON.parse(msg);
 		switch (msg.type) {
-			case "RTCMessage":
-				webSocketServer.broadcast(connection, JSON.stringify(msg));
+			case "request":
+				handleRequest(connection, msg.requestID, msg.data);
 				break;
-			case "cmd":
-				handleCmdMessage(connection, msg);
+			case "response":
+				RTCRequestCallbacks[msg.responseID](msg);
 				break;
 			default:
-				console.error("Unrecognised msg type: ", msg.type);
+				console.error("Unrecognised msg type: " + msg.type);
 		}
 	});
 	connection.on('close', function () {
@@ -54,24 +54,81 @@ webSocketServer.on('connection', function (connection) {
 	});
 });
 
-function handleCmdMessage(connection, msg) {
+function handleCmdMessage(connection, requestID, msg) {
 	switch (msg.cmd) {
-		case "getClients":
-			handleGetClientsCommand(connection, msg);
+		case "getPeers":
+			handleGetPeersCommand(connection, requestID, msg);
 			break;
 		default:
-			console.log("Unrecognised command: ", msg.cmd);
+			console.log("Unrecognised command: " + msg.cmd);
 	}
 }
 
-function handleGetClientsCommand(connection, msg) {
-	var response = {
-		clients: [],
-		type: "getClientsResponse"
+function handleRequest(connection, requestID, msg) {
+	switch (msg.type) {
+		case "RTCMessage":
+			handleRTCRequest(connection, requestID, msg.recipient, msg.data);
+			break;
+		case "cmd":
+			handleCmdMessage(connection, requestID, msg);
+			break;
+		default:
+			throw new Error("Unknown request type: " + msg.data.type);
+	}
+}
+
+function handleRTCRequest(connection, requestID, recipient, msg) {
+	var senderID = connection.id;
+	switch (msg.type) {
+		case "offer":
+			makeAnswerRequest(senderID, recipient, msg, function (reply) {
+				// TODO: Set up reply
+				reply.requestID = requestID;
+				delete reply['responseID'];
+				connection.send(JSON.stringify(reply));
+			});
+			break;
+		case "candidate":
+			var reply = {
+				candidate: msg.candidate,
+				type: "candidate",
+				from: senderID
+			};
+			connections[recipient].send(JSON.stringify(reply));
+
+			break;
+		default:
+			throw new Error("Unknown msg type: " + msg.type);
+	}
+}
+
+
+function makeAnswerRequest(sender, recipient, msg, callback) {
+	var serverRequestID = RNG.generate();
+	delete msg['requestID'];
+	var answer = {
+		from: sender,
+		data: msg,
+		serverRequestID: serverRequestID,
+		type: "request"
 	};
+
+	RTCRequestCallbacks[serverRequestID] = callback;
+	connections[recipient].send(JSON.stringify(answer));
+
+}
+
+function handleGetPeersCommand(connection, requestID, msg) {
+
+	var response = {
+		peers: [],
+		requestID: requestID,
+		type: "response"
+	};
+
 	for (var i in connections) {
 		if (connections[i] !== connection) {
-			response.clients.push(i);
+			response.peers.push(i);
 		}
 	}
 	connection.send(JSON.stringify(response));
--- client/js/webrtc.js
+++ client/js/webrtc.js
@@ -1,3 +1,7 @@
+// TODO: Clean up some of the closures to make code more functional and easier to read.
+
+
+
 var hostname = location.origin.replace(/^http/, 'ws');
 
 var IS_CHROME = !!window.webkitRTCPeerConnection,
@@ -15,159 +19,304 @@ if (IS_CHROME) {
   RTCSessionDescription = mozRTCSessionDescription;
 }
 
-var connection = new RTCPeerConnection({
-	iceServers: [
-		{ 
-			'url': 'stun:23.21.150.121'// (IS_CHROME ? 'stun:stun.l.google.com:19302' : 'stun:23.21.150.121')
-		}
-	],
-
-});
-
-
-connection.messagehandler = function (signallingChannel, msg) {
-	var thisConnection = this;
-	if (msg.sdp && msg.type === "offer") {
-		this.setRemoteDescription(new RTCSessionDescription(msg));
-		thisConnection.createAnswer(function (description) {
-			thisConnection.setLocalDescription(description);
-			signallingChannel.sendRTCMessage(JSON.stringify(description));
-		}, function () {}, {});	
-	} else if (msg.sdp && msg.type === "answer") {
-		this.setRemoteDescription(new RTCSessionDescription(msg));	
-	} else if (msg.candidate) {
-		this.addIceCandidate(new RTCIceCandidate(msg.candidate));
-	} else {
-		console.log(msg);
-	}
-};
-
-connection.onicecandidate = function (event) {
-    if (!event || !event.candidate) {
-    	return;	
-    } else {
-    	this.signallingChannel.sendRTCMessage(JSON.stringify({
-    		candidate: event.candidate
-    	}));
-    }
-};
-
-connection.onerror = function (err) {
-	console.log(err);
-};
-
-connection.ondatachannel = function (event) {
-	window.dataChannel = event.channel;
-
-	setupDataChannel(window.dataChannel);
-};
-
-
-var SignallingChannel = function (peerConnection) {
-	var signallingChannel = this;
-
-	peerConnection.signallingChannel = this;
-
-	this.peerConnection = peerConnection;
-
-	this.webSocketConnection = new WebSocket(hostname);
-
-
-	window.onbeforeunload = function() {
-    signallingChannel.webSocketConnection.close();
+(function (window) {
+	var RNG = function () {};
+	RNG.generate = function () {
+		return Math.floor(Math.random() * 1000000000);
 	};
 
-	this.messageBuffer = [];
+	window.RNG = RNG;
+})(window);
 
-	this.webSocketConnection.onopen = function () {
-		signallingChannel.messageBuffer.map(function (msg) {
-			signallingChannel.send(msg);
+(function (window) {
+
+	var RTCConfig = {
+		iceServers: [
+			{ 
+				'url': 'stun:23.21.150.121'// (IS_CHROME ? 'stun:stun.l.google.com:19302' : 'stun:23.21.150.121')
+			}
+		]
+	};
+
+	var hostname = location.origin.replace(/^http/, 'ws');
+
+
+
+
+	var Coordinator = function () {
+		var thisCoordinator = this;
+
+		this.peers = {}; // There may be false sharing!
+		this.webSocketConnection = new WebSocket(hostname);
+		this.webSocketConnection.messageBuffer = [];
+
+		window.onbeforeunload = function() {
+	    	signallingChannel.webSocketConnection.close();
+		};
+
+		this.requestCallbacks = {};
+		// TODO: Some mechanism to detect requests that don't get
+		// 		 a response
+
+
+		this.webSocketConnection.onmessage = function (response) {
+			var data = response.data;
+			var msg = JSON.parse(data);
+
+
+			var callback = thisCoordinator.requestCallbacks[msg.requestID];
+			switch (msg.type) {
+				case "response":
+					callback(msg);
+					break;
+				case "request":
+					var serverRequestID = msg.serverRequestID;
+					handleRequest(serverRequestID, msg.from, msg.data);
+					break;
+				case "candidate":
+					handleCandidate(msg.from, msg.candidate);
+					break;
+				default:
+					console.log("Unrecognised Message Type!");
+			}
+		}
+
+		function handleRequest(serverRequestID, from, msg) {
+			switch (msg.type) {
+				case "offer":
+					handleOffer(serverRequestID, from, msg);
+					break;
+				case "answer":
+					throw new Error("An answer is not supposed to be handled in this function!");
+				default:
+					throw new Error("Unrecognised data type in handleRequest: ", msg.type);
+			}
+		}
+
+		function handleOffer(serverRequestID, from, msg) {
+			var currentPeer;
+			if (thisCoordinator.peers[from] !== undefined) {
+				currentPeer = thisCoordinator.peers[from];
+			} else {
+				currentPeer = new Peer(thisCoordinator, from);
+			}
+
+			thisCoordinator.peers[from] = currentPeer;
+			currentPeer.generateAnswer(msg, function (description) {
+				thisCoordinator._respondToServer(serverRequestID, description);
+			});
+		}
+
+		function handleCandidate(senderID, candidate) {
+			if (thisCoordinator.peers[senderID]) {
+				thisCoordinator.peers[senderID].connection.addIceCandidate(new RTCIceCandidate(candidate));
+			} else {
+				// Need to somehow buffer candidates
+				throw new Error("Peer object not created yet!");
+			}
+
+		}
+
+
+		this.webSocketConnection.onopen = function () {
+			thisCoordinator.webSocketConnection.messageBuffer.map(function (msg) {
+				thisCoordinator.webSocketConnection.send(msg);
+			});
+		};
+
+		this.getPeers(function (response) {
+			var peers = response.peers;
+			thisCoordinator.connectToPeers(peers);
 		});
 	};
 
-	this.webSocketConnection.onmessage = function (msg) {
-		var data = JSON.parse(msg.data);
-		console.log(data);
+	Coordinator.prototype.connectToPeers = function (peers) {
+		var thisCoordinator = this;
+		peers.map(function (peer) {
+			if (peer in thisCoordinator.peers) {
+				return;
+			} else {
+				thisCoordinator.peers[peer] = new Peer(thisCoordinator, peer);
+				thisCoordinator.peers[peer].initiateConnection();
+			}
+		});
+	};
 
-		if (data.type === "RTCMessage") {
-			peerConnection.messagehandler(signallingChannel, JSON.parse(data.data));
-		} else if (data.type === "getClientsResponse") {
-			signallingChannel.getClientsCallback(data.clients);
-			console.log(data.clients);
-		} else {
-			console.log("Unrecognised data type: ", data.type);
+	Coordinator.prototype.getPeers = function (callback) {
+		this._requestFromServer({
+			type: "cmd",
+			cmd: "getPeers"
+		}, callback);
+	};
+
+	Coordinator.prototype.sendRTCMessage = function (recipient, msg, callback) {
+		this._requestFromServer({
+			recipient: recipient,
+			data: msg,
+			type: "RTCMessage"
+		}, callback);
+	};
+
+	Coordinator.prototype.broadcast = function (msg) {
+		for (peer in this.peers) {
+			peer.send(msg);
 		}
 	};
 
-	this.webSocketConnection.onerror = function (err) {
-		console.log(err);
+	Coordinator.prototype.send = function (recipient, msg) {
+		this.peers[recipient].send(msg);
 	};
 
-};
 
-SignallingChannel.prototype.send = function (msg) {
-	if (typeof msg === "object") {
-		msg = JSON.stringify(msg);
+	Coordinator.prototype._requestFromServer = function (msg, callback) {
+		if (typeof msg !== "object") {
+			throw new Error("Unable to send strings over to the server. Please send JSON requests");
+		} 
+
+		var request = {
+			data: msg,
+			requestID: RNG.generate(),
+			type: "request"
+		};
+
+		this.requestCallbacks[request.requestID] = callback;
+
+		request = JSON.stringify(request);
+
+		// If webSocketConnection is not ready,
+		// buffer the message and wait for ready before
+		// sending
+
+		if (this.webSocketConnection.readyState === 0) {
+			this.webSocketConnection.messageBuffer.push(request);
+		} else {
+			this.webSocketConnection.send(request);
+		}
+
+	};
+
+	Coordinator.prototype._respondToServer = function (serverRequestID, msg) {
+		if (typeof msg !== "object") {
+			throw new Error("Unable to send strings over to the server. Please send JSON requests");
+		}
+
+		var response = {
+			data: msg,
+			responseID: serverRequestID,
+			type: "response"
+		};
+
+		response = JSON.stringify(response);
+
+		// If webSocketConnection is not ready,
+		// buffer the message and wait for ready before
+		// sending (Shouldn't happen in this case since
+		// a request has to be send before a response can
+		// be made, but maybe might want to do some buffering
+
+		if (this.webSocketConnection.readyState === 0) {
+			this.webSocketConnection.messageBuffer.push(response);
+		} else {
+			this.webSocketConnection.send(response);
+		}
+
+	};
+
+	var Peer = function (coordinator, id) {
+		this.coordinator = coordinator;
+		this.connection = new RTCPeerConnection(RTCConfig);
+
+		var thisPeer = this;
+		this.connection.ondatachannel = function (event) {
+			var dataChannel = event.channel;
+			thisPeer.dataChannel = dataChannel;
+			setupDataChannel(dataChannel);
+		};
+		this.id = id;
+	};
+
+	Peer.prototype.initiateConnection = function () {
+
+		var thisPeer = this;
+
+		this.dataChannel = this.connection.createDataChannel(this.id, (IS_CHROME ? {reliable: false} : {}));
+
+		setupDataChannel(this.dataChannel);
+
+		this.connection.createOffer(function (description) {
+			thisPeer.connection.setLocalDescription(description);
+			thisPeer.coordinator.sendRTCMessage(thisPeer.id, description, function (response) {
+				var description = response.data;
+				thisPeer.connection.setRemoteDescription(new RTCSessionDescription(description));
+			});
+		}, function () {});
+
+		this.connection.onicecandidate = function (event) {
+		    if (!event || !event.candidate) {
+		    	return;	
+		    } else {
+		    	thisPeer.coordinator.sendRTCMessage(thisPeer.id, {
+		    		candidate: event.candidate,
+		    		type: "candidate",
+		    		recipient: thisPeer.id
+		    	});
+		    }
+		};
+	};
+
+	Peer.prototype.generateAnswer = function (offerDescription, callback) {
+		var thisConnection = this.connection;
+		var thisPeer = this;
+
+		this.connection.setRemoteDescription(new RTCSessionDescription(offerDescription));
+		this.connection.createAnswer(function (description) {
+			thisConnection.setLocalDescription(description);
+			callback(description);
+		}, function () {});
+
+		this.connection.onicecandidate = function (event) {
+		    if (!event || !event.candidate) {
+		    	return;	
+		    } else {
+		    	thisPeer.coordinator.sendRTCMessage(thisPeer.id, {
+		    		candidate: event.candidate,
+		    		type: "candidate",
+		    		recipient: thisPeer.id
+		    	});
+		    }
+		};
+	};
+
+	Peer.prototype.send = function (msg) {
+		if (!this.dataChannel || this.dataChannel.readyState === "connecting") {
+			// Buffer message?
+		} else {
+			this.dataChannel.send(msg);
+		}
+	};
+
+
+	function setupDataChannel(dataChannel) {
+		dataChannel.onmessage = messageDispatcher;
+
+		dataChannel.onopen = function (event) {
+			console.log("DataChannel open");
+		};
 	}
-	if (this.webSocketConnection.readyState === 0) {
-		this.messageBuffer.push(msg);
-	} else {
-		this.webSocketConnection.send(msg);
-	}
-};
 
-SignallingChannel.prototype.getClients = function (callback) {
-	this.send({
-		type: "cmd",
-		cmd: "getClients"
-	});
-	this.getClientsCallback = callback;
-};
-
-
-SignallingChannel.prototype.sendRTCMessage = function (msg, recipient) {
-	this.send({
-		data: msg,
-		type: "RTCMessage",
-		recipient: recipient
-	});
-};
-
-var signalChannel = new SignallingChannel(connection);
-signalChannel.getClients(function (clients) {
-	console.log(clients);
-});
-
-
-function initiateConnection() {
-
-	createDataChannel();
-
-	connection.createOffer(function (description) {
-		connection.setLocalDescription(description);
-		signalChannel.sendRTCMessage(JSON.stringify(description));
-	}, function () {}, {});
-}
-
-function createDataChannel() {
-	dataChannel = connection.createDataChannel("RTCDataChannel", (IS_CHROME ? {reliable: false} : {}));
-
-
-	setupDataChannel(dataChannel);
-
-}
-
-function setupDataChannel(dataChannel) {
-	dataChannel.onmessage = function (event) {
+	function messageDispatcher(event) {
 		var data = event.data;
 		console.log("I got msg: ", data);
-	};
+	}
 
-	dataChannel.onopen = function (event) {
-		console.log("OPEN");
-	};
-}
 
+
+
+
+	window.Coordinator = Coordinator;
+
+
+})(window);
 
 
 
